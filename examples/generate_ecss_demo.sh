01a440d0c71176e1d2259a89125216257b6dc05a
Michał Kurowski <mkurowski@n7space.com>
2025-12-08 18:49:11 +0100

    Aspects of each component POC

--- examples/generate_ecss_demo.sh
+++ examples/generate_ecss_demo.sh
@@ -2,3 +2,5 @@
 mkdir -p output
 template-processor --verbosity info --iv ../data/requirements.iv.xml -o output -t ../data/ecss-template/ecss-e-st-40c_6_requirement_traceability.tmplt
 pandoc --pdf-engine=pdfroff --output=output/ecss-e-st-40c_6_requirement_traceability.pdf output/ecss-e-st-40c_6_requirement_traceability.md
+template-processor --verbosity info --iv ../data/simple.iv.xml -o output -t ../data/ecss-template/ecss-e-st-40c_5_4_aspects_of_each_component.tmplt
+pandoc --pdf-engine=pdfroff --output=output/ecss-e-st-40c_5_4_aspects_of_each_component.pdf output/ecss-e-st-40c_5_4_aspects_of_each_component.md
