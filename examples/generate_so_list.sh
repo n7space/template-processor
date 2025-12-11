f2c01bd9136acd863493659850e916614791c717
Michał Kurowski <mkurowski@n7space.com>
2025-12-11 02:25:57 +0100

    Improved System Objects example

--- examples/generate_so_list.sh
+++ examples/generate_so_list.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p output
 template-processor --verbosity info --sos ../data/events.csv -o output -t so_list.tmplt
+pandoc --pdf-engine=pdfroff --output=output/so_list.pdf output/so_list.md
